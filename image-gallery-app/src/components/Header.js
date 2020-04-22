c74db0cf7433fc5296024db67abdecec1a2a723c
Ian Schwartz <ian.schwartz23@gmail.com>
2020-04-22 15:19:10 -0700

    clean up, created header, initial styling

---
+++ image-gallery-app/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="jumbotron text-center">
+      <h1 className="title">Unsplash Image Gallery</h1>
+    </div>
+  );
+};
+
+export default Header;
