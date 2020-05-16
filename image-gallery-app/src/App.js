8ea9e96bdc1e0b111a5ada32ebf553f0a96d2cfd
Ian Schwartz <ian.schwartz23@gmail.com>
2020-05-16 14:29:06 -0700

    optimized axios request

--- image-gallery-app/src/App.js
+++ image-gallery-app/src/App.js
@@ -8,39 +8,56 @@ import { HashLoader } from "react-spinners";
 
 function App() {
   const [images, setImages] = useState([]);
+  const [searchImages, setSearchImages] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [page, setPage] = useState(1);
+  const [term, setTerm] = useState("");
 
   useEffect(() => {
     axios
       .get(
-        `https://api.unsplash.com/photos/?client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}&per_page=30&page=${page}`
+        term === ""
+          ? `https://api.unsplash.com/photos/?client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}&per_page=30&page=${page}`
+          : `https://api.unsplash.com/search/photos/?client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}&per_page=30&page=${page}&query=${term}`
       )
       .then((res) => {
-        const photos = res.data;
-        console.log("Photos", photos);
-        setImages(photos);
+        term === "" ? setImages(res.data) : setSearchImages(res.data.results);
         setIsLoading(false);
+        // console.log("Photos", res.data);
+        // console.log("Search Photos", res.data.results);
       })
       .catch((err) => console.log("The data was not returned", err));
-  }, [page]);
+  }, [term, page]);
 
   return (
     <div>
       <Header />
-      <ImageSearch />
+      <ImageSearch
+        page={page}
+        setPage={setPage}
+        term={term}
+        setTerm={setTerm}
+      />
       <Pagination page={page} setPage={setPage} />
       <div>
-        {!isLoading && images.length === 0 && <h1>No Images Found</h1>}
+        {!isLoading && term !== "" && searchImages.length === 0 && (
+          <h1 className="not-found-text">No Images Found</h1>
+        )}
 
         {isLoading ? (
           <div className="spinner">
             <HashLoader color={"#f2f2f2"} loading={isLoading} size={175} />
           </div>
-        ) : (
+        ) : term === "" ? (
           <div className="card-wrap">
             {images.map((image) => (
               <ImageCard key={image.id} image={image} />
+            ))}
+          </div>
+        ) : (
+          <div className="card-wrap">
+            {searchImages.map((searchImage) => (
+              <ImageCard key={searchImage.id} image={searchImage} />
             ))}
           </div>
         )}
--- image-gallery-app/src/components/ImageSearch.js
+++ image-gallery-app/src/components/ImageSearch.js
@@ -1,28 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
-import axios from "axios";
-import ImageCard from "./ImageCard";
 
-const ImageSearch = () => {
-  const [searchImages, setSearchImages] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+const ImageSearch = ({ setTerm }) => {
   const [text, setText] = useState("");
-  const [term, setTerm] = useState("");
-  const [page, setPage] = useState(1);
-
-  useEffect(() => {
-    axios
-      .get(
-        `https://api.unsplash.com/search/photos/?client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}&per_page=30&query=${term}&page=${page}`
-      )
-      .then((res) => {
-        const searchedPhotos = res.data;
-        console.log("Searched Photos", searchedPhotos);
-        setSearchImages(searchedPhotos);
-        setIsLoading(false);
-      })
-      .catch((err) => console.log("The data was not returned", err));
-  }, [term, page]);
 
   const handleChange = (e) => {
     setText(e.target.value);
@@ -63,16 +43,6 @@ const ImageSearch = () => {
           Search
         </Button>
       </Form>
-      {/* {!isLoading && searchImages.results.length === 0 && <h1 className="title">No Images Found</h1>} */}
-      {isLoading ? (
-        <></>
-      ) : (
-        <div className="card-wrap">
-          {searchImages.results.map((image) => (
-            <ImageCard key={image.id} image={image} />
-          ))}
-        </div>
-      )}
     </div>
   );
 };
